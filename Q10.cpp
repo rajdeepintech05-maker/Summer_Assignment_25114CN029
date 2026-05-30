f60cf8e89b846f9fcee9e9d96751b7f4da6dce69
Rajdeep Shukla <rajdeepintech05@gmail.com>
2026-05-30 16:51:00 +0530

    Add files via upload

---
+++ Q10.cpp
@@ -0,0 +1,35 @@
+// Q10 : Write a program to Print prime numbers in a range.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start, end, i, j;
+    bool isPrime;
+
+    cout << "Enter starting number: ";
+    cin >> start;
+
+    cout << "Enter ending number: ";
+    cin >> end;
+
+    cout << "Prime numbers between " << start << " and " << end << " are:\n";
+
+    for (i = start; i <= end; i++) {
+        if (i <= 1)
+            continue;
+
+        isPrime = true;
+
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            cout << i << " ";
+    }
+
+    return 0;
+}
