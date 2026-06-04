a14a4b2bd9b9e7faa9f3dd0eb253553d0b57f094
Rajdeep Shukla <rajdeepintech05@gmail.com>
2026-06-05 03:49:46 +0530

    Add files via upload

---
+++ DAY - 8/Q29.cpp
@@ -0,0 +1,19 @@
+// Q29: Write a program to Print half pyramid pattern.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
